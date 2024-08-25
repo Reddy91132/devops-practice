497f2663750b6c60c7e68af56d63f84cee1452dc
yuvasai07 <reddy123hon85@gmail.com>
2024-08-25 23:13:01 +0530

    i added new conversation

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 
-person1::sharath
-person2::bhaskar
-person3::ramya
-person4::meghana
+PERSON1=sharath
+PERSON2=bhaskar
+PERSON3=ramya
+PERSON4=meghana
 
-echo $person1::"lee {$person1} mall, ge ogona barthiya"
-echo $person2::"lee evattu {$person3} nanu film oktha edhivibaralla nanu"
-echo $person1::"{$person3} jothe adre okthiya nan johe adre baralla ala"
-echo $person2::"sorry lee {$person4} na kelu barthana" 
+echo "$person1::lee ${PERSON2}mall, ge ogona barthiya"
+echo "$person2::lee evattu,${PERSON3} nanu film oktha edhivibaralla nanu"
+echo "$person1::${PERSON3},jothe adre okthiya nan johe adre baralla ala"
+echo "$person2::sorry lee,${PERSON4} na kelu barthana" 
