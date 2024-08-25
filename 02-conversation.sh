4e1904f3eeae6faf3a894df134dd865c3b1b1169
yuvasai07 <reddy123hon85@gmail.com>
2024-08-25 22:35:57 +0530

    i added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+echo yuvasai::"lee sanjay mall, ge ogona barthiya"
+echo sanjay::"lee evattu kavya nanu film pktha edhivibaralla nanu"
+echo yuvasai::"kavya jothe adre okthiya nan johe adre baralla ala"
+echo sanjaya::"sorry lee" 
