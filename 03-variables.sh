d376b724e91edd8707aefae370bb6b088a3bdce1
yuvasai07 <reddy123hon85@gmail.com>
2024-08-25 22:52:58 +0530

    i added another conversation

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+person1::sharath
+person2::bhaskar
+person3::ramya
+person4::meghana
+
+echo $person1::"lee sanjay mall, ge ogona barthiya"
+echo $person2::"lee evattu $person3 nanu film pktha edhivibaralla nanu"
+echo $person1::"$person3 jothe adre okthiya nan johe adre baralla ala"
+echo $person2::"sorry lee $person4 na kelu barthana" 
