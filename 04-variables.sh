4b2165ea95b76b0d8e93abe2e4334f82d2995e16
yuvasai07 <reddy123hon85@gmail.com>
2024-08-26 10:44:15 +0530

    i added variables

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+PERSON4=$4
+
+echo "$PERSON1:: lee ${PERSON2} mall, ge ogona barthiya"
+echo "${PERSON2}:: lee evattu,${PERSON3} nanu film oktha edhivibaralla nanu"
+echo "$PERSON1::${PERSON3},jothe adre okthiya nan johe adre baralla ala"
+echo "$PERSON2::sorry lee,${PERSON4} na kelu barthala"
