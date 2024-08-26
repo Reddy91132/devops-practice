67aca55e40d7acdfa772c6ba7c14713b230f3e1a
yuvasai07 <reddy123hon85@gmail.com>
2024-08-26 18:26:05 +0530

    i added special variable

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -5,3 +5,5 @@ echo "script name : $0"
 echo "current working directory : $PWD"
 echo "home directory of current user : $HOME"
 echo "PID of the executing script : $$"
+sleep 100&
+echo "PID last executing script : $$"
