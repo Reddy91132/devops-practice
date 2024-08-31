82df3d1b06c9e6c8da1c961f8c6bb3422132ec2c
yuvasai07 <reddy123hon85@gmail.com>
2024-08-31 11:42:07 +0530

    backup

---
+++ 17-backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bas/
+
+SORCE_DIR=$1
+DESTI_DIR=$2
+DAYS=${3:-14} #if they give $3 consider,if they not give 14 days will continues
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+USAGE() {
+    echo "USAGE :: 19-backup.sh <source> <destination> <days(optional)>"
+}
+if [ $# -lt 2]
+then
+
+  USAGE
+  
+  fi
