065ad9b30afea97f10bb6b5aa2b52315bd18b04f
yuvasai07 <reddy123hon85@gmail.com>
2024-08-31 19:06:07 +0530

    backup

--- 18-disk-usage.sh
+++ 18-disk-usage.sh
@@ -7,7 +7,7 @@ DISK_THRESHOLD=5 #REAL PROJECT IS 75
   do
   USAGE=$(echo $line  df -hT |grep xfs |awk -F " " '{print $6F}'|cut -d "$" -f1)
   PARTITION=$(echo $line  df -hT |grep xfs |awk -F " " '{print $NF}')
-  if [$USAGE -ge $DISK_THRESHOLD ]
+  if [ $USAGE -ge $DISK_THRESHOLD ]
    then
      echo "$PARTITION IS more than $DISK_THRESHOLD,current valve $USAGE.PLEASE CHECK"
     fi
